98212f776f8e5bd7ff2dc67322508e987d21c21d
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-04-21 18:30:18 +0300

    Refactor tests into class testsuite format, add tests for error messages. Drop some tests for the update constructor for now.

--- tests/ConstructorTest.m
+++ tests/ConstructorTest.m
@@ -1,222 +1,139 @@
-% test cube with 4 bands and non-square dimensions (size [2,3,4])
-cube   = cat(3, ones(2,3), 2*ones(2,3), 3*ones(2,3), 4*ones(2,3));
+classdef ConstructorTest < matlab.unittest.TestCase
+    % ConstructorTest tests the Cube constructor api and resulting object
+    % properties.
 
-% test metadata matching the cube
-qty   = 'Quantity';
-wlu   = 'Wavelength unit';
-wls   = [0, 3, 1, 2];
-fwhms = [2, 1, 0.5, 3];
+    properties 
+         % expected default values for missing parameters for the cube
+        defaultQuantity       = 'Unknown';
+        defaultWavelengthUnit = 'Band index';
+        defaultUnknownUnit    = 'Unknown';
+        
+        % constant test data cube and corresponding metadata
+        data = ones(2,3,4,'double');
+        wl   = [1 3 5 7];
+    end
+    
+    properties (TestParameter)
+        type = {'double', ...
+                'single', ...
+                'int8',  'uint8',...
+                'int16', 'uint16',...
+                'int32', 'uint32',...
+                'int64', 'uint64'...
+                };
+        
+        % Size parameters
+        width  = struct('column', 1, ...
+                        'small',  2, ...
+                        'large', 10  ...
+                        );
+        height  = struct('row',    1, ...
+                         'small',  2, ...
+                         'large', 10  ...
+                         );
+        bands  = struct('image', 1, ...
+                        'rgb',   3, ...
+                        'hsi', 100  ...
+                        );
+        bands2 = struct('image', 1, ...
+                        'rgb',   3, ...
+                        'hsi',  50  ...
+                        );
+        % Test metadata
+        qty   = {'A', 'Quantity'};
+        wlu   = {'nm', 'Wavelength unit'};
+    end
 
-% expected default values for missing parameters for the cube
-default_qty  = 'Unknown';
-default_wlu  = 'Band index';
-default_wlu_with_given_wl = 'Unknown';
-default_wl   = 1:size(cube,3);
-default_fwhm = zeros(1, size(cube,3));
+    methods (Test)
 
-%% Test NullConstructor
-assert(isempty(Cube().Data))
-assert(isempty(Cube().Bands))
-assert(isempty(Cube().Wavelength))
-assert(isempty(Cube().FWHM))
+        function testNullConstructor(testCase)
+            % (only) the null constructor should result in empty properties
+            nullCube = Cube();
+            testCase.verifyEmpty(nullCube.Data);
+            testCase.verifyEmpty(nullCube.Bands);
+            testCase.verifyEmpty(nullCube.Wavelength);
+            testCase.verifyEmpty(nullCube.FWHM);
+        end
+        
+        function testDataIdentity(testCase, width, height, bands, type)
+            % Cube should retain the data as is
+            cube = ones(height, width, bands, type);
+            c = Cube(cube);
+            testCase.verifyClass(c.Data, type);
+            testCase.verifyEqual(c.Type, type);
+        end
+        
+        function testMetaSize(testCase, width, height, bands)
+            % Wavelength, FWHM sizes should match the data and nBands prop.
+            cube = ones(height, width, bands);
+            c = Cube(cube);
+            testCase.verifyLength(c.Wavelength, bands);
+            testCase.verifyLength(c.FWHM, bands);
+            testCase.verifyEqual(c.nBands, bands);
+        end
+        
+        function testWavelengthMismatch(testCase, width, height, bands, bands2)
+            % Mismatched wavelength vector size should result in error
+            testCase.assumeNotEqual(bands, bands2);
+            cube = ones(height, width, bands);
+            wls   = 1:bands2;
+            testCase.verifyError(@(x) Cube(cube, 'wl', wls), 'Cube:IncorrectWavelength');
+        end
+        
+        function testFWHMMismatch(testCase, width, height, bands, bands2)
+            % Mismatched fwhm vector size should result in error
+            testCase.assumeNotEqual(bands, bands2);
+            cube = ones(height, width, bands);
+            fwhms   = 1:bands2;
+            testCase.verifyError(@(x) Cube(cube, 'fwhm', fwhms), 'Cube:IncorrectFWHM');
+        end
+        
+        function testDefaultQuantity(testCase)
+            c = testCase.verifyWarning(@(x)Cube(testCase.data), 'Cube:DefaultQuantity');
+            testCase.verifyEqual(c.Quantity, testCase.defaultQuantity);
+        end
+        
+        function testDefaultWavelengthUnit(testCase)
+            c = testCase.verifyWarning(@(x)Cube(testCase.data), 'Cube:DefaultWavelengthUnit');
+            testCase.verifyEqual(c.WavelengthUnit, testCase.defaultWavelengthUnit);
+        end
+        
+        function testDefaultWavelength(testCase, width, height, bands)
+            % Default wavelength vector should be [1, 2, ... , bands]
+            cube = ones(height, width, bands);
+            c = testCase.verifyWarning(@(x)Cube(cube), 'Cube:DefaultWavelength');
+            testCase.verifyEqual(c.Wavelength, 1:bands);
+        end
+        
+        function testUnknownWavelengthUnit(testCase)
+            % If wavelengths are supplied without the unit, it is set to
+            % testCase.defaultUnknownUnit with a warning
+            c = testCase.verifyWarning(@(x)Cube(testCase.data, 'wl', testCase.wl), 'Cube:UnknownWavelengthUnit');
+            testCase.verifyEqual(c.WavelengthUnit, testCase.defaultUnknownUnit);
+        end
+        
+        function testDefaultFWHM(testCase, width, height, bands)
+            % Default FWHM vector should be zeros(1, bands)
+            cube = ones(height, width, bands);
+            c = testCase.verifyWarning(@(x)Cube(cube), 'Cube:DefaultFWHM');
+            testCase.verifyEqual(c.FWHM, zeros(1,bands));
+        end
+        
+        function testUpdateConstructor_Quantity(testCase)
+            c1 = Cube(testCase.data, 'quantity', 'OldQuantity');
+            c2 = Cube(c1, 'quantity', 'NewQuantity');
 
-%% Test TypeIdentity
-c = Cube(double(cube));
-assert(isequal('double', class(c.Data)));
-assert(isequal('double', c.Type));
+            testCase.verifyEqual(c1.Quantity, 'OldQuantity');
+            testCase.verifyEqual(c2.Quantity, 'NewQuantity');
+        end
+        
+        function testUpdateConstructor_WavelengthUnit(testCase)
+            c1 = Cube(testCase.data, 'wlunit', 'OldWavelengthUnit');
+            c2 = Cube(c1, 'wlunit', 'NewWavelengthUnit');
 
-c = Cube(single(cube));
-assert(isequal('single', class(c.Data)));
-assert(isequal('single', c.Type));
-
-c = Cube(int8(cube));
-assert(isequal('int8', class(c.Data)));
-assert(isequal('int8', c.Type));
-
-c = Cube(uint8(cube));
-assert(isequal('uint8', class(c.Data)));
-assert(isequal('uint8', c.Type));
-
-c = Cube(int16(cube));
-assert(isequal('int16', class(c.Data)));
-assert(isequal('int16', c.Type));
-
-c = Cube(uint16(cube));
-assert(isequal('uint16', class(c.Data)));
-assert(isequal('uint16', c.Type));
-
-c = Cube(int32(cube));
-assert(isequal('int32', class(c.Data)));
-assert(isequal('int32', c.Type));
-
-c = Cube(uint32(cube));
-assert(isequal('uint32', class(c.Data)));
-assert(isequal('uint32', c.Type));
-
-c = Cube(int64(cube));
-assert(isequal('int64', class(c.Data)));
-assert(isequal('int64', c.Type));
-
-c = Cube(uint64(cube));
-assert(isequal('uint64', class(c.Data)));
-assert(isequal('uint64', c.Type));
-
-%% Test DataIdentity_cube
-c = Cube(cube);
-assert(isequal(cube, c.Data))
-
-%% Test DataIdentity_im
-im     = [1  2  3  4;
-          5  6  7  8;
-          9 10 11 12];
-c = Cube(im);
-assert(isequal(im, c.Data))
-
-%% Test DataIdentity_rowvec
-rowvec = [1 2 3 4];
-c = Cube(rowvec);
-assert(isequal(rowvec, c.Data))
-
-%% Test DataIdentity_colvec
-colvec = [1 2 3 4]';
-c = Cube(colvec);
-assert(isequal(colvec, c.Data))
-
-%% Test DefaultQuantity
-c = Cube(cube);
-assert(isequal(default_qty, c.Quantity));
-
-c = Cube(cube, 'quantity', qty);
-assert(isequal(qty, c.Quantity));
-
-%% Test DefaultWavelengthUnit
-c = Cube(cube);
-assert(isequal(default_wlu, c.WavelengthUnit));
-
-c = Cube(cube, 'wl', wls);
-assert(isequal(default_wlu_with_given_wl, c.WavelengthUnit));
-
-c = Cube(cube, 'fwhm', fwhms);
-assert(isequal(default_wlu, c.WavelengthUnit));
-
-c = Cube(cube, 'wl', wls, 'fwhm', fwhms);
-assert(isequal(default_wlu_with_given_wl, c.WavelengthUnit));
-
-c = Cube(cube, 'wlunit', wlu);
-assert(isequal(wlu, c.WavelengthUnit));
-
-c = Cube(cube, 'wlunit', wlu, 'wl', wls);
-assert(isequal(wlu, c.WavelengthUnit));
-
-c = Cube(cube, 'wlunit', wlu, 'fwhm', fwhms);
-assert(isequal(wlu, c.WavelengthUnit));
-
-c = Cube(cube, 'wlunit', wlu, 'wl', wls, 'fwhm', fwhms);
-assert(isequal(wlu, c.WavelengthUnit));
-
-%% Test DefaultWavelength
-c = Cube(cube);
-assert(isequal(default_wl, c.Wavelength));
-
-c = Cube(cube, 'wlunit', wlu);
-assert(isequal(default_wl, c.Wavelength));
-
-c = Cube(cube, 'fwhm', fwhms);
-assert(isequal(default_wl, c.Wavelength));
-
-c = Cube(cube, 'wl', wls);
-assert(isequal(wls, c.Wavelength));
-
-c = Cube(cube, 'wl', wls, 'fwhm', fwhms);
-assert(isequal(wls, c.Wavelength));
-
-c = Cube(cube, 'wl', wls, 'wlunit', wlu);
-assert(isequal(wls, c.Wavelength));
-
-c = Cube(cube, 'wl', wls, 'fwhm', fwhms, 'wlunit', wlu);
-assert(isequal(wls, c.Wavelength));
-
-%% Test DefaultFWHM
-c = Cube(cube);
-assert(isequal(default_fwhm, c.FWHM));
-
-c = Cube(cube, 'wlunit', wlu);
-assert(isequal(default_fwhm, c.FWHM));
-
-c = Cube(cube, 'wl', wls);
-assert(isequal(default_fwhm, c.FWHM));
-
-c = Cube(cube, 'fwhm', fwhms);
-assert(isequal(fwhms, c.FWHM));
-
-c = Cube(cube, 'fwhm', fwhms, 'wlunit', wlu);
-assert(isequal(fwhms, c.FWHM));
-
-c = Cube(cube, 'fwhm', fwhms, 'wl', wls);
-assert(isequal(fwhms, c.FWHM));
-
-c = Cube(cube, 'fwhm', fwhms, 'wl', wls, 'wlunit', wlu);
-assert(isequal(fwhms, c.FWHM));
-
-%% Test CopyConstructor
-c1 = Cube(cube, 'quantity', qty, 'wlunit', wlu, 'wl', wls, 'fwhm', fwhms);
-c2 = Cube(c1);
-
-assert(isequal(c1.Data, c2.Data));
-assert(isequal(class(c1.Data), class(c2.Data)));
-assert(isequal(c1.Type, c2.Type));
-assert(isequal(c1.Quantity, c2.Quantity));
-assert(isequal(c1.WavelengthUnit, c2.WavelengthUnit));
-assert(isequal(c1.Wavelength, c2.Wavelength));
-assert(isequal(c1.FWHM, c2.FWHM));
-
-%% Test UpdateConstructor_Quantity
-c1 = Cube(cube, 'quantity', 'OldQuantity');
-c2 = Cube(c1,   'quantity', 'NewQuantity');
-
-assert(isequal(c1.Quantity, 'OldQuantity'));
-assert(isequal(c2.Quantity, 'NewQuantity'));
-
-%% Test UpdateConstructor_WavelengthUnit
-c1 = Cube(cube, 'wlunit', 'OldWavelengthUnit');
-c2 = Cube(c1,   'wlunit', 'NewWavelengthUnit');
-
-assert(isequal(c1.WavelengthUnit, 'OldWavelengthUnit'));
-assert(isequal(c2.WavelengthUnit, 'NewWavelengthUnit'));
-
-%% Test UpdateConstructor_Wavelength
-c1 = Cube(cube, 'wl', 1:4);
-c2 = Cube(c1,   'wl', 2:5);
-assert(isequal(c1.Wavelength, 1:4));
-assert(isequal(c2.Wavelength, 2:5));
-assert(isequal(c1.WavelengthUnit, default_wlu_with_given_wl));
-assert(isequal(c2.WavelengthUnit, default_wlu_with_given_wl));
-
-c1 = Cube(cube, 'wl', 1:4, 'wlunit', wlu);
-c2 = Cube(c1,   'wl', 2:5);
-assert(isequal(c1.Wavelength, 1:4));
-assert(isequal(c2.Wavelength, 2:5));
-assert(isequal(c1.WavelengthUnit, wlu));
-assert(isequal(c2.WavelengthUnit, default_wlu_with_given_wl));
-
-c1 = Cube(cube, 'wl', 1:4, 'wlunit', wlu);
-c2 = Cube(c1,   'wl', 2:5, 'wlunit', wlu);
-assert(isequal(c1.Wavelength, 1:4));
-assert(isequal(c2.Wavelength, 2:5));
-assert(isequal(c1.WavelengthUnit, wlu));
-assert(isequal(c2.WavelengthUnit, wlu));
-
-%% Test UpdateConstructor_FWHM
-c1 = Cube(cube, 'fwhm', [1 1 1 1]);
-c2 = Cube(c1,   'fwhm', [2 2 2 2]);
-assert(isequal(c1.FWHM, [1 1 1 1]));
-assert(isequal(c2.FWHM, [2 2 2 2]));
-assert(isequal(c1.WavelengthUnit, default_wlu));
-assert(isequal(c2.WavelengthUnit, default_wlu));
-
-c1 = Cube(cube, 'fwhm', [1 1 1 1], 'wlunit', wlu);
-c2 = Cube(c1,   'fwhm', [2 2 2 2]);
-assert(isequal(c1.FWHM, [1 1 1 1]));
-assert(isequal(c2.FWHM, [2 2 2 2]));
-assert(isequal(c1.WavelengthUnit, wlu));
-assert(isequal(c2.WavelengthUnit, wlu));
+            testCase.verifyEqual(c1.WavelengthUnit, 'OldWavelengthUnit');
+            testCase.verifyEqual(c2.WavelengthUnit, 'NewWavelengthUnit');
+        end
+        
+    end
+end
