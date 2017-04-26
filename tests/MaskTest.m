841e0bf6f8ca41bf4e33a050e97c07dc56649fad
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-04-26 17:06:25 +0300

    Add input validation tests, reduce redundant tests by initializing maskim only once.

--- tests/MaskTest.m
+++ tests/MaskTest.m
@@ -21,15 +21,13 @@ classdef MaskTest < matlab.unittest.TestCase
                         'multi',    5, ...
                         'hyper',  100  ...
                         );
-                    
-    end
-
-    properties (MethodSetupParameter)
+        
         maskseed = {1, 2, 3, 4, 5};
     end
     
     methods (TestClassSetup)
-        function createCube(testCase, width, height, nbands)
+        function createTestData(testCase, width, height, nbands, maskseed)
+           % Create a test Cube
            data = gallery('uniformdata', [height, width, nbands], 1);
            % Suppress default value warnings for clearer test output
            warning('off', 'Cube:DefaultQuantity');
@@ -39,16 +37,41 @@ classdef MaskTest < matlab.unittest.TestCase
            testCase.testCube = Cube(data);
            % Turn warnings back on
            warning('on', 'all');
-        end
-    end
-    
-    methods (TestMethodSetup)
-        function createMask(testCase, maskseed)
-            testCase.testMask = logical(gallery('integerdata', [0 1], [testCase.testCube.Height, testCase.testCube.Width], maskseed));
+           
+           % Create a test mask matching in size
+           testCase.testMask = logical(gallery('integerdata', [0 1], [height, width], maskseed));
         end
     end
     
     methods (Test)
+        
+        function maskTypeError(testCase)
+            % mask images should be logical matrices
+            im = ones(testCase.testCube.Height, testCase.testCube.Width);
+            testCase.verifyError(@()testCase.testCube.mask(im), 'Cube:InvalidMaskType');
+            
+            im = zeros(testCase.testCube.Height, testCase.testCube.Width);
+            testCase.verifyError(@()testCase.testCube.mask(im), 'Cube:InvalidMaskType');
+        end
+        
+        function maskShapeError(testCase)
+            % mask should error when given a mask with the wrong shape
+            im = true(testCase.testCube.Height + 1, testCase.testCube.Width);
+            testCase.verifyError(@()testCase.testCube.mask(im), 'Cube:InvalidMaskShape');
+            
+            im = true(testCase.testCube.Height - 1, testCase.testCube.Width);
+            testCase.verifyError(@()testCase.testCube.mask(im), 'Cube:InvalidMaskShape');
+            
+            im = true(testCase.testCube.Height, testCase.testCube.Width + 1);
+            testCase.verifyError(@()testCase.testCube.mask(im), 'Cube:InvalidMaskShape');
+            
+            im = true(testCase.testCube.Height, testCase.testCube.Width - 1);
+            testCase.verifyError(@()testCase.testCube.mask(im), 'Cube:InvalidMaskShape');
+            
+            % no (> 2)-dimensional masks either
+            im = true(testCase.testCube.Height, testCase.testCube.Width, 2);
+            testCase.verifyError(@()testCase.testCube.mask(im), 'Cube:InvalidMaskShape');
+        end
         
         function maskArea(testCase)
             % Sum of the true pixels in the mask should match the new area
