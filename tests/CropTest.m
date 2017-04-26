18ce6ace35731dcf56740f05375ef697100f98c8
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-04-26 14:08:05 +0300

    Add a class-based CropTest stub

---
+++ tests/CropTest.m
@@ -0,0 +1,41 @@
+classdef CropTest < matlab.unittest.TestCase
+    
+    properties
+       testCube 
+    end
+    
+    properties (ClassSetupParameter)
+        height = struct('row',    1, ...
+                        'small',  3, ...
+                        'large', 10  ...
+                        )
+                    
+        width  = struct('column', 1, ...
+                        'small',  3, ...
+                        'large', 10  ...
+                        )
+                    
+        nbands = struct('single',   1, ...
+                        'rgb',      3, ...
+                        'multi',    5, ...
+                        'hyper',  100  ...
+                        );
+    end
+
+    methods (TestClassSetup)
+        function createCube(testCase, width, height, nbands)
+           data = gallery('uniformdata', [height, width, nbands], 1);
+           % Suppress default value warnings for clearer test output
+           warning('off', 'Cube:DefaultQuantity');
+           warning('off', 'Cube:DefaultWavelengthUnit');
+           warning('off', 'Cube:DefaultWavelength');
+           warning('off', 'Cube:DefaultFWHM');
+           testCase.testCube = Cube(data);
+           % Turn warnings back on
+           warning('on', 'all');
+        end
+    end
+    
+    methods (Test)
+    end
+end
