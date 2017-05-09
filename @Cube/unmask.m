f07acce306fe5d7d2c12a5246511a54b55697479
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-05-09 17:56:39 +0300

    Improve input validation for unmask()

--- @Cube/unmask.m
+++ @Cube/unmask.m
@@ -2,18 +2,21 @@ function [obj, maskim] = unmask(obj,maskim)
 %UNMASK Spatially reshape columnwise ordered spectra
 % unmask(maskim) attempts to reorganize the Cube data into
 % a Cube with the spatial dimensions of the mask image, with
-% the nonempty pixels in the mask containing the data and the
-% rest initialized to zero (inverse of mask() for the preserved
-% points).
+% the true values in the mask containing the data and the
+% rest initialized to zero.
+% 
 % The matrix maskim must be a logical matrix with the amount of
-% nonempty elements equal to the number of pixels in the Cube.
+% true elements equal to the number of pixels in the Cube.
 
-assert(islogical(maskim), 'The mask must be a logical matrix.');
+assert(islogical(maskim), 'Cube:InvalidMaskType', 'The mask must be a logical matrix.');
+% scalars are accepted for the edge case of a single pixel
+assert(ismatrix(maskim) || isscalar(maskim), 'Cube:InvalidMaskShape', 'The mask must be a logical matrix.');
 
 % Calculate the number of spatial pixels and check for sanity
 N = sum(maskim(:));
 
 assert(N == obj.Area,...
+    'Cube:InvalidMaskArea', ...
     'The number of nonempty pixels in the mask (%d) does not equal the number of existing pixels (%d)',...
     N,obj.Area);
 
