26b96c913154b0ca7d76b7f2f37d37462c177ddb
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-05-09 17:20:32 +0300

    Separate unmask to a separate file, add maskim as output parameter

--- @Cube/Cube.m
+++ @Cube/Cube.m
@@ -367,36 +367,8 @@ classdef Cube
         % Spatial masking. See mask.m
         [obj, maskim] = mask(obj,maskim)
         
-        
-        function obj = unmask(obj,maskim)
-            %UNMASK Spatially reshape columnwise ordered spectra
-            % unmask(maskim) attempts to reorganize the Cube data into
-            % a Cube with the spatial dimensions of the mask image, with
-            % the nonempty pixels in the mask containing the data and the
-            % rest initialized to zero (inverse of mask() for the preserved
-            % points).
-            % The matrix maskim must be a logical matrix with the amount of
-            % nonempty elements equal to the number of pixels in the Cube.
-            
-            assert(islogical(maskim), 'The mask must be a logical matrix.');
-            
-            % Calculate the number of spatial pixels and check for sanity
-            N = sum(maskim(:));
-            
-            assert(N == obj.Area,...
-                'The number of nonempty pixels in the mask (%d) does not equal the number of existing pixels (%d)',...
-                N,obj.Area);
-            
-            % Replicate the mask for each band for easy indexing
-            cubeMask = repmat(maskim,1,1,obj.nBands);
-            
-            % Initialize the new data with zero and assign the values to
-            % the mask positions
-            tmp           = zeros(size(cubeMask), obj.Type);
-            tmp(cubeMask) = obj.Data(:);
-            obj.Data      = tmp;
-            obj.History   = {{'Reshaped using a mask image', @unmask, maskim}};
-        end
+        % Unmasking back to a cube. See unmask.m
+        [obj, maskim] = unmask(obj,maskim)
         
         function obj = px(obj,x,y)
             %PX Select pixels from the cube
---
+++ @Cube/unmask.m
@@ -0,0 +1,30 @@
+function [obj, maskim] = unmask(obj,maskim)
+%UNMASK Spatially reshape columnwise ordered spectra
+% unmask(maskim) attempts to reorganize the Cube data into
+% a Cube with the spatial dimensions of the mask image, with
+% the nonempty pixels in the mask containing the data and the
+% rest initialized to zero (inverse of mask() for the preserved
+% points).
+% The matrix maskim must be a logical matrix with the amount of
+% nonempty elements equal to the number of pixels in the Cube.
+
+assert(islogical(maskim), 'The mask must be a logical matrix.');
+
+% Calculate the number of spatial pixels and check for sanity
+N = sum(maskim(:));
+
+assert(N == obj.Area,...
+    'The number of nonempty pixels in the mask (%d) does not equal the number of existing pixels (%d)',...
+    N,obj.Area);
+
+% Replicate the mask for each band for easy indexing
+cubeMask = repmat(maskim,1,1,obj.nBands);
+
+% Initialize the new data with zero and assign the values to
+% the mask positions
+tmp           = zeros(size(cubeMask), obj.Type);
+tmp(cubeMask) = obj.Data(:);
+obj.Data      = tmp;
+obj.History   = {{'Reshaped using a mask image', @unmask, maskim}};
+
+end
