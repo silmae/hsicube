deaac0d50c4ec1f8b1c2e40f15d794cc466027ad
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-05-12 16:40:47 +0300

    Add tests for the spatial mean

---
+++ tests/MeanTest.m
@@ -0,0 +1,67 @@
+classdef MeanTest < matlab.unittest.TestCase
+    
+    properties
+        testCube
+    end
+    
+    properties (ClassSetupParameter)
+        height = struct('row',    1, ...
+                        'small',  3, ...
+                        'large', 10  ...
+                        )
+                    
+        width  = struct('column', 1, ...
+                        'small',  3, ...
+                        'large', 10  ...
+                        )
+                    
+        nbands = struct('single',   1, ...
+                        'rgb',      3, ...
+                        'multi',    5, ...
+                        'hyper',  100  ...
+                        );
+    end
+
+    methods (TestClassSetup)
+        function createCube(testCase, width, height, nbands)
+           data = gallery('uniformdata', [height, width, nbands], 1);
+           % Suppress default value warnings for clearer test output
+           warning('off', 'Cube:DefaultQuantity');
+           warning('off', 'Cube:DefaultWavelengthUnit');
+           warning('off', 'Cube:DefaultWavelength');
+           warning('off', 'Cube:DefaultFWHM');
+           testCase.testCube = Cube(data);
+           % Turn warnings back on
+           warning('on', 'all');
+        end
+    end
+    
+    methods (Test)
+        
+        function meanResultWidth(testCase)
+            % Width should be 1 after taking the mean
+            m = testCase.testCube.mean;
+            testCase.verifyEqual(m.Width, 1);
+        end
+        
+        function meanResultHeight(testCase)
+            % Height should be 1 after taking the mean
+            m = testCase.testCube.mean;
+            testCase.verifyEqual(m.Height, 1);
+        end
+        
+        function meanPreservesBands(testCase)
+            % Number of bands should not change
+            nb   = testCase.testCube.nBands;
+            m = testCase.testCube.mean;
+            testCase.verifyEqual(m.nBands, nb);
+        end
+        
+        function meanIdempotent(testCase)
+            % mean twice should equal taking the mean once
+           once  = testCase.testCube.mean.Data;
+           twice = testCase.testCube.mean.mean.Data;
+           testCase.verifyEqual(twice, once);
+        end
+    end
+end
