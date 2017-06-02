bbcdbf234d2946b06063106cc280fbd53ff5a227
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-06-02 12:29:07 +0300

    Move utility functions to a static class for cleaner namespace

---
+++ @Utils/Utils.m
@@ -0,0 +1,9 @@
+classdef Utils
+    methods (Static)
+        % True for integers of any numeric type
+        [bool] = isint(A)
+        
+        % True for integers > 0
+        [bool] = isnatural(a)
+    end
+end
