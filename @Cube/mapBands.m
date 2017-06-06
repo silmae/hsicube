0bdc9dd5dd374ce230385ac0e6ec32b8b8278fc5
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-06-06 12:14:00 +0300

    mapBands: remove history parameter for simplicity, add default value for quantity

--- @Cube/mapBands.m
+++ @Cube/mapBands.m
@@ -1,23 +1,21 @@
-function obj = mapBands(obj, f, history, qty)
+function obj = mapBands(obj, f, qty)
 %MAPBANDS Apply a function on each band of the data
-% mapBands(f,history,qty) applies f on each band (image) and
-% returns a Cube with the resulting data. User must supply a
-% history string to be included in the result history, and the
-% quantity specification for the new data.
-% Changes in wavelength and fwhm values are not supported,
-% though the spatial dimensions of the data may change.
+% mapBands(f, qty) applies f on each band (image) and
+% returns a Cube with the resulting data. The result quantity will be set
+% to qty. If the quantity is not supplied, it will be set to 'Unknown'.
 
 assert(isa(f,'function_handle'),...
     'Expected a valid function handle');
-assert(CubeArgs.isValidQuantity(qty),...
-    'Quantity specification must be a non-empty char array');
 
 for b = obj.Bands
     tmp(:,:,b) = f(obj.Data(:,:,b));
 end
 
+if nargin < 3
+    qty = 'Unknown';
+end
+
 obj.Data     = tmp;
 obj.Quantity = qty;
-obj.History  = {{'Applied function on each band',...
-    f, history, qty}};
+obj.History  = {{'Applied function on each band',f}};
 end
