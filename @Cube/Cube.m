d9488116384e738c51c1a81cbfbc15b8d6d8ded5
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-06-26 17:02:32 +0300

    Cube: Separate take into take.m

--- @Cube/Cube.m
+++ @Cube/Cube.m
@@ -319,21 +319,8 @@ classdef Cube
         % Pixel selection. See px.m
         [obj, cx] = px(obj,cx)
         
-        function obj = take(obj,n)
-            %TAKE Take the first n pixel spectra (in column order)
-            % Usage: TAKE(N), where n = 1,...,Area returns a cube
-            % containing the first n pixels in a single column.
-            % If n > Area, returns all the pixels.
-            assert(Utils.isnatural(n), ...
-                'Cube:InvalidAmount', 'N must be a natural number');
-            
-            if n > obj.Area
-                n = obj.Area;
-            end
-            
-            obj.History = {{'Taking n first pixels', @take, n}};
-            obj = obj.byCols.px([ones(n,1),(1:n)']);
-        end
+        % Take a number of spectra (pixels). See take.m
+        [obj] = take(obj,n)
         
         %% Arithmetic operations %%
         % 
