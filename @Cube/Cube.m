dc181f7b5bd0c5e226a7efafc0cbce558066be47
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-06-26 16:14:07 +0300

    Cube: Update take to use the new px syntax and add msgid to error check

--- @Cube/Cube.m
+++ @Cube/Cube.m
@@ -321,17 +321,18 @@ classdef Cube
         
         function obj = take(obj,n)
             %TAKE Take the first n pixel spectra (in column order)
-            % Usage: take(N), where n = 1,...,Area returns a cube
+            % Usage: TAKE(N), where n = 1,...,Area returns a cube
             % containing the first n pixels in a single column.
             % If n > Area, returns all the pixels.
-            assert(Utils.isnatural(n), 'N must be a natural number');
+            assert(Utils.isnatural(n), ...
+                'Cube:InvalidAmount', 'N must be a natural number');
             
             if n > obj.Area
                 n = obj.Area;
             end
             
             obj.History = {{'Taking n first pixels', @take, n}};
-            obj = obj.byCols.px(ones(1,n)',(1:n)');
+            obj = obj.byCols.px([ones(n,1),(1:n)']);
         end
         
         %% Arithmetic operations %%
