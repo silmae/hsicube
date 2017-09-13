a27f80ae38e86ff096acf58636bf273b4a699953
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-09-13 12:23:58 +0300

    Fix comment on toList

--- @Cube/toList.m
+++ @Cube/toList.m
@@ -1,6 +1,6 @@
 function obj = toList(obj)
 %TOLIST Reshape the Cube to a list of spectra
-% c2 = c1.byCols reshapes the W x H x Bands Cube c1 to an
+% c2 = c1.toList reshapes the W x H x Bands Cube c1 to an
 % (W * H) x 1 x Bands cube c2. The spectra are ordered in column-first
 % order (MATLAB default).
 % 
