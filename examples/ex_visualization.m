e8807f7b8da5b0ac02e41dfa54bf674381839003
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-09-19 15:25:52 +0300

    Add example stubs for array ops, map usage, data selection and visualization

---
+++ examples/ex_visualization.m
@@ -0,0 +1,10 @@
+%% Visualizing data
+% The Cube class includes some useful wrappers for MATLAB visualizations
+% to instantly visualize data cubes with the meaningful ticks and labeling.
+
+%% Viewing bands as images
+
+
+%% Using HIST()
+
+%% Using 
