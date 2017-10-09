c301ae09b880765231c5c437c17c26ede3bd2162
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-10-09 16:04:38 +0300

    Add example of a histogram visualization

--- examples/ex_visualization.m
+++ examples/ex_visualization.m
@@ -1,10 +1,20 @@
 %% Visualizing data
-% The Cube class includes some useful wrappers for MATLAB visualizations
-% to instantly visualize data cubes with the meaningful ticks and labeling.
+% For data visualization, the Cube class contains various functions that
+% wrap existing MATLAB functionality in a content-aware way for each type
+% of data.
 
-%% Viewing bands as images
+house = ENVI.read('house_uint16_rad.dat', 'Radiance');
 
+%% HSI histograms
+% The hist() method calculates histograms for each band in the data using
+% MATLAB histcounts, and displays them as a image with a colormap.
 
-%% Using HIST()
+house.hist('count');
 
-%% Using 
+%%
+% By default, the bins are set automatically based on the whole dataset
+% (with a single binning for all the bands). To set a custom binning, we
+% can pass the bin edges as a parameter.
+
+bin_edges = 0:65536;
+house.hist('count', bin_edges);
