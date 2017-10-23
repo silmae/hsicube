74b1bbd571c0df0d50f7faa1b4bb77054543bc4c
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-10-23 15:52:06 +0300

    Add default behaviour for single-band images and extra labeling

--- @Cube/im.m
+++ @Cube/im.m
@@ -7,13 +7,18 @@ function [obj,im] = im(obj, b)
 % If colorbrewer is found, the *GnBu colormap will be used instead of the
 % default parula.
 
-assert(isscalar(b), ...
-    'Cube:InvalidSingleBand', ...
-    'You must supply a single band index for visualization');
+if nargin < 2 && obj.nBands == 1
+    b = 1;
+else
+    assert(isscalar(b), ...
+        'Cube:InvalidSingleBand', ...
+        'You must supply a single band index for visualization');
+end
 obj = obj.bands(b);
 h = figure();
 im = imagesc(squeeze(obj.Data));
 axis image;
+title(sprintf('Band %d (%f%s)', b, obj.Wavelength(1), obj.WavelengthUnit));
 
 if exist('brewermap.m', 'file') == 2
     colormap(brewermap(100, '*GnBu'));
@@ -21,6 +26,6 @@ else
     colormap(parula);
 end
 
-colorbar(gca);
-
+cb = colorbar(gca);
+cb.Label.String = obj.Quantity;
 end
